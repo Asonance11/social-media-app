728aaaeaeb8a853cce4d0ae55f1956aecf8fbc5e
Oluwadabira <adigunsamuelaso@gmail.com>
2023-07-03 14:33:34 +0100

    feat: Setting up routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
 function App() {
-	return <div className="text-2xl text-green-700 text-center">App</div>;
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/signup" element={<Signup />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
