f5c19df91545c1118cb6e14466c89783c50d11f0
Oluwadabira <adigunsamuelaso@gmail.com>
2023-07-09 17:37:02 +0100

    Navbar component content

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,19 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<nav className="py-4 px-8 flex justify-between items-center">
+			<Link to="/">Logo</Link>
+			<div>
+				<Link to="/login">
+					<button type="button">Login</button>
+				</Link>
+				<Link to="/signup">
+					<button type="button">Signup</button>
+				</Link>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
