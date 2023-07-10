7f19bc99277d8bdf13b1964fa4c9f992a3380306
Oluwadabira <adigunsamuelaso@gmail.com>
2023-07-10 18:45:29 +0100

    change navbar if user is logged in

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,55 @@
 import { Link } from 'react-router-dom';
+import UserAuth from '../context/AuthContext';
 
 const Navbar = () => {
+	const { user, logout } = UserAuth();
+
+	const handleLogout = async () => {
+		try {
+			await logout();
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
 	return (
 		<nav className="px-[5%] py-4 flex justify-between items-center font-roboto bg-lotion text-eerieBlack md:px-8">
 			<Link to="/">
 				<h1 className="text-3xl text-darkBlue">Logo</h1>
 			</Link>
-			<div className="flex items-center gap-4">
-				<Link to="/login">
+			{user?.email ? (
+				<div className="flex items-center gap-4">
+					<p className="text-base font-medium text-darkBlue capitalize">
+						{user?.displayName}
+					</p>
 					<button
 						type="button"
 						className="py-2 px-4 bg-darkBlue rounded-xl text-base font-medium text-white"
+						onClick={handleLogout}
 					>
-						Login
+						Logout
 					</button>
-				</Link>
-				<Link to="/signup">
-					<button
-						type="button"
-						className="hidden border border-darkBlue py-2 px-3 rounded-xl text-base font-medium text-darkBlue md:block"
-					>
-						Signup
-					</button>
-				</Link>
-			</div>
+				</div>
+			) : (
+				<div className="flex items-center gap-4">
+					<Link to="/login">
+						<button
+							type="button"
+							className="py-2 px-4 bg-darkBlue rounded-xl text-base font-medium text-white"
+						>
+							Login
+						</button>
+					</Link>
+					<Link to="/signup">
+						<button
+							type="button"
+							className="hidden border border-darkBlue py-2 px-3 rounded-xl text-base font-medium text-darkBlue md:block"
+						>
+							Signup
+						</button>
+					</Link>
+				</div>
+			)}
 		</nav>
 	);
 };
