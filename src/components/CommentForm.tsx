5439a9751846fd0d862b0a557040d708ec949923
Oluwadabira <adigunsamuelaso@gmail.com>
2023-07-27 22:08:54 +0100

    feat: Users can create comments

--- src/components/CommentForm.tsx
+++ src/components/CommentForm.tsx
@@ -1,10 +1,45 @@
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
 import { useState } from 'react';
+import { toast } from 'sonner';
+import { db } from '../config/firebase';
+import UserAuth from '../context/AuthContext';
 
-const CommentForm = () => {
+interface PropsType {
+	postId: string;
+}
+
+const CommentForm = ({ postId }: PropsType) => {
 	const [commment, setComment] = useState('');
 
+	const { user } = UserAuth();
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		try {
+			const commentRef = addDoc(
+				collection(db, 'posts', `${postId}`, 'comments'),
+				{
+					username: user?.displayName,
+					comment: commment,
+					timestamp: serverTimestamp(),
+				}
+			);
+			await commentRef;
+			setComment('');
+		} catch (error) {
+			console.error(error);
+			toast.error(`${error}`);
+		}
+	};
+
 	return (
-		<form className="mt-2 flex items-center w-full gap-2 px-2">
+		<form
+			className="mt-2 flex items-center w-full gap-2 px-2"
+			onSubmit={(e) => {
+				handleSubmit(e);
+			}}
+		>
 			<input
 				type="text"
 				name="comment"
