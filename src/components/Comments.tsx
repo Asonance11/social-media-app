ca871e197e814472823e6abbd985d55ffbceeaa3
Oluwadabira <adigunsamuelaso@gmail.com>
2023-10-16 14:57:02 +0100

    refactor: putting the code in a useeEffect

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -3,57 +3,56 @@ import { useEffect, useState } from 'react';
 import { toast } from 'sonner';
 import { db } from '../config/firebase';
 
-interface CommentsType {
+interface Comment {
 	comment: string;
 	username: string;
 	id: string;
 }
-interface PropsType {
+interface Props {
 	postId: string;
 }
 
-const Comments = ({ postId }: PropsType) => {
-	const [comments, setComments] = useState<CommentsType[]>([]);
-
-	const commentCollectionRef = collection(db, 'posts', `${postId}`, 'comments');
-	const commmentCollectionQuery = query(
-		commentCollectionRef,
-		orderBy('timestamp', 'desc')
-	);
-
-	const getComments = async () => {
-		try {
-			if (postId) {
-				// @ts-ignore
-				const unsub = onSnapshot(commmentCollectionQuery, (snapshot) => {
-					// console.log(snapshot.docs);
-					setComments(
-						snapshot.docs.map(
-							(doc) =>
-								({
-									...doc.data(),
-									id: doc.id,
-								} as CommentsType)
-						)
-					);
-				});
-			}
-		} catch (error) {
-			console.error(error);
-			toast.error(`${error}`);
-		}
-	};
+const Comments = ({ postId }: Props) => {
+	const [comments, setComments] = useState<Comment[]>([]);
 
 	useEffect(() => {
-		getComments();
-	});
+		const commentCollectionRef = collection(db, 'posts', postId, 'comments');
+		const commentCollectionQuery = query(
+			commentCollectionRef,
+			orderBy('timestamp', 'desc')
+		);
+
+		try {
+			const unsubscribe = onSnapshot(commentCollectionQuery, (snapshot) => {
+				setComments(
+					snapshot.docs.map(
+						(doc) =>
+							({
+								...doc.data(),
+								id: doc.id,
+							} as Comment)
+					)
+				);
+			});
+
+			// Clean up the listener when the component unmounts
+			return () => {
+				unsubscribe();
+			};
+		} catch (error) {
+			if (error instanceof Error) {
+				console.error(error);
+				toast.error(`Error fetching comments: ${error.message}`);
+			}
+		}
+	}, [postId]);
 
 	return (
 		<section className="mt-2 px-2">
-			{comments.map((comm) => (
-				<div key={comm.id} className="flex items-center gap-2 mt-2 text-sm">
-					<strong>{comm.username}</strong>
-					<p>{comm.comment}</p>
+			{comments.map((comment) => (
+				<div key={comment.id} className="flex items-center gap-2 mt-2 text-sm">
+					<strong>{comment.username}</strong>
+					<p>{comment.comment}</p>
 				</div>
 			))}
 		</section>
